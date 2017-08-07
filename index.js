b6f5b9f0af1afdaa1e6d17418d3d4e7e91485e7c
zachpflederer <zach@fastmodeltechnologies.com>
2017-08-07 15:15:20 -0500

    fix: lib dir requires all files

--- index.js
+++
@@ -1,9 +0,0 @@
-import generateActions from './src/actions.js';
-import generateActionCreators from './src/actionCreators.js';
-import generateReducers from './src/reducers.js';
-
-export {
-  generateActions,
-  generateActionCreators,
-  generateReducers,
-};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import generateActions from './actions.js';
+import generateActionCreators from './actionCreators.js';
+import generateReducers from './reducers.js';
+
+export {
+  generateActions,
+  generateActionCreators,
+  generateReducers,
+};
